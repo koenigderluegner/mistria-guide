f89ead397c146a03cf20d0ba550c3f5e8010d3cc
Alexander Niersmann <aniersmann@gmail.com>
2025-06-01 14:23:20 +0200

    basic skills and perks design

--- apps/frontend/src/app/skills/skills.component.ts
+++ apps/frontend/src/app/skills/skills.component.ts
@@ -1,15 +1,35 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
 import { httpResource } from '@angular/common/http';
-import { Skill, SkillId } from '@mistria-guide/data-types';
-import { JsonPipe } from '@angular/common';
+import { Perk, Skill, SkillId, SkillIds } from '@mistria-guide/data-types';
 import { SpriteComponent } from '../shared/sprite/sprite.component';
 
 @Component({
   selector: 'app-skills',
-  imports: [JsonPipe, SpriteComponent],
+  imports: [SpriteComponent],
   templateUrl: './skills.component.html',
 })
 export class SkillsComponent {
-  itemId = input<string>();
-  item = httpResource<Record<SkillId, Skill>>('database/skills.json');
+  skillId = input<SkillId>();
+  protected skills = httpResource<Record<SkillId, Skill>>(
+    'database/skills.json'
+  );
+  protected foundSkill = computed(() => {
+    if (!this.skills.hasValue()) return undefined;
+
+    const keys = Object.keys(this.skills.value()) as SkillId[];
+    const skillId = this.skillId();
+    const shownSkillId =
+      skillId && SkillIds.includes(skillId) ? skillId : keys[0];
+    return this.skills.value()[shownSkillId];
+  });
+
+  tiers = computed<Perk[][]>(() => {
+    const skill = this.foundSkill();
+    if (!skill) return [];
+    const tierKeys = Object.keys(skill)
+      .filter((key) => key.startsWith('tier'))
+      .sort();
+    // @ts-expect-error cant be index with type string, TODO fix
+    return tierKeys.map((key) => skill[key] as Perk[]);
+  });
 }
