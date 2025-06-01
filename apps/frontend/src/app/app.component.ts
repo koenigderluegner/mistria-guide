0e08eb041ecc72624119d403b8d93c61c72db7e2
Alexander Niersmann <aniersmann@gmail.com>
2025-06-01 11:08:56 +0200

    register github icon

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { SidebarContainerComponent } from './sidebar-container/sidebar-container.component';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   imports: [RouterModule, SidebarContainerComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent {}
+export class AppComponent {
+  #matIconRegistry = inject(MatIconRegistry);
+  #domSanitizer = inject(DomSanitizer);
+
+  constructor() {
+    this.#matIconRegistry.addSvgIcon(
+      'github-mark',
+      this.#domSanitizer.bypassSecurityTrustResourceUrl(
+        'assets/ui/github-mark.svg'
+      )
+    );
+  }
+}
