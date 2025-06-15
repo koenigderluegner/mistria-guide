a2ea790d2b5e09d131b42d8d735e354f31ac1818
Alexander Niersmann <aniersmann@gmail.com>
2025-06-15 11:15:42 +0200

    install dev kit for nx

--- apps/parser/src/environments/environment.ts
+++ apps/parser/src/environments/environment.ts
@@ -1,28 +1,19 @@
 import { EnvironmentConfig } from './environment-config.interface';
 import path from 'path';
+import { workspaceRoot } from 'nx/src/utils/workspace-root';
 
 export const environment: EnvironmentConfig = {
   assetPath: path.join(__dirname, '..', 'assets'),
   frontendDbPath: path.join(
-    __dirname,
-    '..',
-    '..',
-    '..',
-    '..',
-    '..',
+    workspaceRoot,
     'apps',
     'frontend',
     'public',
     'database'
   ),
   generatedOutputPath: path.join(__dirname, '..', 'generated'),
-  generatedDataTypesPath:path.join(
-    __dirname,
-    '..',
-    '..',
-    '..',
-    '..',
-    '..',
+  generatedDataTypesPath: path.join(
+    workspaceRoot,
     'libs',
     'data-types',
     'src',
